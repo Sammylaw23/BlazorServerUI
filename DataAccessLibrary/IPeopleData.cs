c0cceb6f2a45c3b565c7544c1ab4f5f22a731164
Sammylaw23 <lawalsamuel2310@gmail.com>
2021-05-20 07:10:40 +0100

    First Commit

---
+++ DataAccessLibrary/IPeopleData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IPeopleData
+    {
+        Task InsertPerson(PersonModel person);
+        Task<List<PersonModel>> GetPeople();
+    }
+}
---
+++ SqlDataAccess/IPeopleData.cs
@@ -0,0 +1,12 @@
+﻿using SqlDataAccess.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SqlDataAccess
+{
+    public interface IPeopleData
+    {
+        Task InsertPerson(PersonModel person);
+        Task<List<PersonModel>> LoadPeople();
+    }
+}
